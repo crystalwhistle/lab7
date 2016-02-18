9113a457fbda7b4c6faed7399dfca751962279ec
Crystal <crystal1770@Gmail.com>
2016-02-18 05:59:14 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,15 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	projects["grid"] = false;
+  	res.render('index', projects);
+};
+
+/*
+ * GET home page.
+ */
+
+exports.viewGrid = function(req, res){
+	projects["grid"] = true;
   	res.render('index', projects);
 };
