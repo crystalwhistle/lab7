71f7074ba999821ffcc12c6f5229dc3eefaa5449
Crystal <crystal1770@Gmail.com>
2016-02-18 09:28:53 +0000

    grid experiment

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num >= 0) {
+	  projects['grid'] = false;
+	  res.render('index', projects);
+	} else {
+	  res.redirect('/grid');
+	}
+
 	projects["grid"] = false;
   	res.render('index', projects);
 };
